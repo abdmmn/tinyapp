85bcbdf36fe1d1191364db82718559026b58985c
abdmmn <abduljmemon@gmail.com>
2023-03-22 20:27:47 +0000

    update readme

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# tinyapp
-Url shortening service
+TinyApp Project
+TinyApp is a full stack web application built with Node and Express that allows users to shorten long URLs (à la bit.ly).
+
+Final Product
+Main login page screenshot
+User account of all URLs with edit and delete options
+"look inside screenshot folder"
+
+Dependencies
+Node.js
+Express
+EJS
+bcryptjs
+body-parser
+cookie-session
+cookie-parser
+
+Getting Started
+Install all dependencies (using the npm install command).
+Run the development web server using the node express_server.js command.
--- express_server.js
+++ express_server.js
@@ -19,8 +19,34 @@ app.use(cookieSession({
 }))
 
 /*-----Functions-----------------------------------------------*/
-const { getUserByEmail, generateRandomString, urlsForUser} = require('./helpers')
+// const { getUserByEmail, generateRandomString, urlsForUser} = require('./helpers')
 
+function getUserByEmail(email, database) {
+
+  for (let id in database) {
+    if (database[id].email === email) {
+      return id;
+    }
+  } return false;
+}
+
+function generateRandomString() {
+  const chars = '0123456789abcdefghijklmnopqrstuvwxyz'
+  let result = '';
+  for (let i = 6; i > 0; i--) {
+    result += chars[Math.floor(Math.random() * (chars.length))];
+  }
+  return result;
+}
+
+const urlsForUser = function (urlDatabase, id) {
+  const userUrls = {};
+  for (let key in urlDatabase) {
+    if (urlDatabase[key].userID === id) {
+      userUrls[key] = urlDatabase[key]
+    }
+  } return userUrls
+}
 /*-----Database------------------------------------------------*/
 
 const urlDatabase = {
